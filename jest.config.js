0e42c785435ebb3b808b2790096652af18323867
Kunal Kachru <kachrukunal414@gmail.com>
2026-07-11 06:52:12 +0200

    feat: install react-native-webview for the 3D product viewer
    Verified with a clean build + launch smoke test on both an Android
    emulator and the iOS Simulator before any feature code was added.

--- jest.config.js
+++ jest.config.js
@@ -5,6 +5,6 @@ module.exports = {
     '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
   },
   transformIgnorePatterns: [
-    'node_modules/(?!(react-native|@react-native|@react-navigation|react-redux|@reduxjs|redux-persist|immer|reselect|redux|redux-thunk|react-native-reanimated|react-native-gesture-handler|react-native-screens|react-native-safe-area-context|react-native-vector-icons|react-native-paper|react-native-picker-select|@react-native-async-storage|@react-native-community|@react-native-picker|react-native-worklets|axios)/)',
+    'node_modules/(?!(react-native|@react-native|@react-navigation|react-redux|@reduxjs|redux-persist|immer|reselect|redux|redux-thunk|react-native-reanimated|react-native-gesture-handler|react-native-screens|react-native-safe-area-context|react-native-vector-icons|react-native-paper|react-native-picker-select|@react-native-async-storage|@react-native-community|@react-native-picker|react-native-worklets|axios|react-native-webview)/)',
   ],
 };
